61da6004bdd9d4e120c60d40dafb93cf5dbf9eb0
AlanGlezH <47785262+AlanGlezH@users.noreply.github.com>
2020-07-07 23:13:32 -0500

    bytes transfer from server to client implemented

--- streaming/client.rb
+++ streaming/client.rb
@@ -4,19 +4,34 @@ $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
 require 'grpc'
 require 'stream_services_pb'
+require "zlib"
 
 def  main
-    hostname = 'localhost:7000'
+    hostname = 'localhost:8000'
     stub = Stream::StreamingService::Stub.new(hostname, :this_channel_is_insecure)
     begin
-        message = stub.get_track_audio(Stream::TrackRequest.new(idTrack: '12334', quality: nil))
-        print "Enviando: #{message.audio}"
-       
-
+        track_sample = stub.get_track_audio(Stream::TrackRequest.new(idTrack: '12345', quality: nil))
     rescue GRPC::BadStatus => e
         abort "Error: #{e.message}"
     end
-    
-end
+   
+    #File (Sin comprimir)
+    Zlib::GzipWriter.open('./media/track2.bin') do |destin_file|
+        destin_file.write track_sample.audio  
+    end
 
+    
+
+ 
+    
+
+   
+
+  
+
+
+
+
+end
+    
 main
--- streaming/server.rb
+++ streaming/server.rb
@@ -7,15 +7,19 @@ $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 require 'grpc'
 require 'stream_services_pb'
 
-class StreamingServer < Stream::StreamingService::Service
+class StreamingServer < Stream::StreamingService::Service    
     def get_track_audio(track_request,_unused_call)
         idTrack = track_request.idTrack
         print 'El track es: ',idTrack
-        
-        track_sample = Stream::TrackSample.new(audio: "1010101")
-        puts track_sample.audio
+        track_sample = Stream::TrackSample.new(audio: nil)
+
+        File.open "./media/track1.mp3", "r" do |source_file|
+            until source_file.eof?
+                chunk = source_file.read 100000 
+                track_sample.audio = chunk + track_sample.audio
+            end
+        end
         return track_sample
-        
     end
     
 end
@@ -23,7 +27,7 @@ end
 
 def main
     server = GRPC::RpcServer.new
-    server.add_http2_port('0.0.0.0:7000', :this_port_is_insecure)
+    server.add_http2_port('0.0.0.0:8000', :this_port_is_insecure)
     server.handle(StreamingServer)
     print 'Grpc streaming server is running'
     server.run_till_terminated_or_interrupted([1, 'int', 'SIGQUIT'])
