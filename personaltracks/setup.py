09974d3f2c74de017faa7f0747f165d886663ccd
Victor Niño <ninomtz.victor@gmail.com>
2020-07-19 20:31:14 -0500

    setup added for personal tracks module

---
+++ personaltracks/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="personaltracks",
+    version="0.1.0",
+    description="Personal tracks manager module",
+    packages=find_packages()
+)
